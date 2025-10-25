a10f1415c60ee0965d2887f06ae2d2b7c8f85285
Narthana Prasanth <narthanaprasanth24@gmail.com>
2025-10-25 09:34:07 +0530

    first commit

--- obj.js
+++ obj.js
@@ -550,67 +550,376 @@ let classObj = {
 
 
 //SUBJECT WITH HIGHEST PERCENTAGE OF MARKS
-let lowestpercentage=Infinity
-let lowestsubject=null
+// let subjecttotals={}
+// let subjectcount={}
 
-classObj.student.forEach(stu=>{
-    stu.marks.forEach(s=>{
-    let totalmark=s.reduce((a,b)=>a+b.mark,0)
-    console.log(totalmark)
-    })              
-})
+// classObj.student.forEach(stu=>{
+//     stu.marks.forEach(s=>{
+//         let sub=s.subject
+
+//         if(!subjecttotals[sub]){
+//             subjecttotals[sub]=0
+//             subjectcount[sub]=0
+//         }
+//         subjecttotals[sub]+=s.mark
+//         subjectcount[sub]++
+//     })
+// })
+
+// let highestpercentage=0
+// let topsubject=null
+// for(let sub in subjecttotals){
+//     let totalpossible=subjectcount[sub]*50
+//     let percentage=(subjecttotals[sub]/totalpossible)*100
+
+//     if(percentage>highestpercentage){
+//         highestpercentage=percentage
+//         topsubject=sub
+//     }
+// }
+
+// console.log(`subject with highest percentage:${highestpercentage.toFixed(0)}%:${topsubject}`)
+
+
+//STUDENT WITH LOWEST PERCENTAGE OF MARK
+// let subjecttotal={}
+// let subjectcount={}         
+
+// classObj.student.forEach(stu=>{
+//     stu.marks.forEach(s=>{
+//         let sub=s.subject
+
+//         if(!subjecttotal[sub]){
+//             subjecttotal[sub]=0
+//             subjectcount[sub]=0
+//         }
+//         subjecttotal[sub]+=s.mark
+//         subjectcount[sub]++
+//     }) 
+// })
+//  let lowestpercentage=Infinity  
+//  let lowsubject=null
+//  for(let sub in subjecttotal){
+//     let totalpossible=subjectcount[sub]*50
+//     let percentage=(subjecttotal[sub]/totalpossible)*100
+// if(percentage<lowestpercentage){
+//     lowestpercentage=percentage
+//     lowsubject=sub
+// }
+// }
+// console.log(`subject with lowest percentage:${lowestpercentage.toFixed(0)}%:${lowsubject}`)
+
+ 
+//STUDENT WITH LOWEST PERCENTAGE OF MARK IN SPECIFIC SUBJECT
+// let lowestpercentage=Infinity
+// let studentname=null
+// let subjectname="English"
+//  classObj.student.forEach(stu=>{
+//     let sub=stu.marks.find(s=>s.subject===subjectname)
+//     let percentage=(sub.mark/50)*100
+//     if(percentage<lowestpercentage){
+//         lowestpercentage=percentage
+//         studentname=stu.name
+//     }
+    
+//  })
+//  console.log(`student with lowest percentage of mark ${studentname}:${lowestpercentage}%`)
+
+
+
+                                      
+//HIGHER ORDER FUNCTION IN JS
+// function higherorder(fn){
+//     fn()
+// } 
+ 
+// higherorder(function(){console.log("hi")})
 
 
    
+     //CALL()
+        
+// function say(message){
+//     return this.name +" "+ "hai" +" "+ message
+// }                                                              
+// var person={name:"jia"}
+//  console.log(say.call(person,"hello"))
+           
+                                                                                                                                                              
+
+                        
+         //APPLY()
+// function say(message){
+//     return this.name+" " +"hi"+ " "+ message
+// }
+// let person={
+//     name:"gis"
+// }
+// console.log(say.apply(person,['goodmorning']))
+        
+
+    //BIND
+    // let person={
+    //     name:"john"
+    // }
+    // function say(message){
+    //     return this.name+" "+"hi"+" "+ message
+    // }
+    // let bounded=say.bind(person)
+    // console.log(bounded("gooodiws"))
+
+    //CURRYING
+
+    //   function add(a){
+    //     return function(b){
+    //         return a+b
+    //     }
+
+    //   }
+      
+    // console.log(add(3)(4))      
+                     
+
+           
+                
+// function random(){
+//     var obj1={
+//         name:"vivan",
+//         age:20
+//     }
+
+//     return function(){
+//         console.log(obj1.name+" "+"is"+" "+"awsome")
+//     }
+// }
+// var initialclocsure=random()
+// initialclocsure()
+
+   
+
+//CALL BACK
+// function divide(sum){
+//     console.log(sum/2)
+
+// }
+// function multily(sum){
+//     console.log(sum*2)
+// }
+// function operation(num1,num2,operation){
+//     let sum=num1+num2
+//     operation(sum)
+// }
+// operation(2,3,divide)
+// operation(10,2,multily)
  
+
+    //  function hai(){
+    //     console.log("orginal")
+    //  }
+    //  function gia(callback){
+    //     console.log("after thia")
+    //     callback()
+    //  }
+    //  gia(hai)
+
+
+
+ //RECURSION   
+    // function add(number){
+    //     if(number<0){
+    //         return 0
+    //     }else{
+    //         return number + add(number-1)
+    //     }
+    // }
+    // console.log(add(3))
+
+      
+//  function add(arr){
+//     if(arr.length===1){
+//         return arr[0]
+//     }else{
+//         return arr.pop()+add(arr)
+//     }
+
+//  }
+//  console.log(add([10,20,30,40]))
+
+//CONSTRUCTOR
+
+// function person(name,age,gender){
+//     this.name=name
+//     this.age=age
+//     this.gender=gender
+// }
+// var person1= new person("inku",20,"female")
+// console.log(person1)
+                       
+// var person2=new person("hike",10,"male")
+// console.log(person2)
  
+
+  //ARROW FUNCTION
+//    var multipy=function(num){
+//     return num*2
+//    }
+//  console.log(multipy(2))
+
+// var multipy=num=>num*2
+// console.log(multipy(2))
+
+
+                
+            
+   //PROMISE          
  
+// function sumofthreeelement(...element){
+//     return new Promise((resolve,reject)=>{
+//         if(element.length>3){
+//             reject("only 3 elemnt is allowed")
+//         }else{
+//             let sum=0
+//             let i=0
+//             while(i<element.length){
+//                 sum+=element[i]
+//                 i++
+//             }
+//             resolve("sum is "+ sum)
+//         }
+//     })
+
+// }
+// sumofthreeelement(1,2,3)
+//     .then((message)=>{
+//         console.log(message)
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
+
+    //GENERATIVE FUNCTION
+    
+// function *getfunc(){
+//     yield 3
+//     yield 4
+// }   
+// console.log(getfunc().next())
+
+
+        
+           
+                                   
+//DESTRUCTURING
+// const classdetails={
+//     strength:78,
+//     benches:39,
+//     blackboard:1
+// }
+// const {strength:classstrength,benches:classbenches,blackboard:classblackboard}=classdetails
+// console.log(classbenches)
+                                  
+//BINARY SEARCH ON SORTED ARRAY
+      
  
-          
+//  function binarysearch(arr,target){
+//     let low=0
+//     let high=arr.length-1
+//     while(low<high){
+//         let mid=Math.floor((low+high)/2)
+//       if(arr[mid]=target){
+//         return mid
+//       }else if(arr[mid]<target){
+//         low=mid+1
+//       }else{
+//         high=mid-1
+//       }
+//     }
+//     return -1
+
+//  }
+// let ind= binarysearch([1,2,3,4,5,6,7],4)
+// console.log(`target is in the index ${ind}`)                                
+
+             
  
-                                                        
- 
- 
- 
+// function binarysearch(arr,target){
+//     let low=0
+//     let high=arr.length-1
+//     while(low<=high){
+//         let mid=Math.floor((low+high)/2)
+//         if(arr[mid]===target){
+//             return mid
+//         }else if(arr[mid]<target){
+//             low=mid+1
+//         }else{
+//             high=mid-1
+//         }
+//     }
+//     return -1
+// }
+    
+// const arr=[10,20,30,11,22,33,44]
+// let sortedarr=arr.sort((a,b)=>a-b)
+// console.log(sortedarr)
+// console.log(binarysearch(sortedarr,33))
+
+
+//RIGHT ROTATION N TIMES
+// function rightrotation(arr,rotation){
+//     if(rotation===0) return arr
+//     for(let i=0;i<rotation;i++){
+//         let element=arr.pop()
+//         arr.unshift(element)
+//     }
+//     return arr
+// }
+// console.log(rightrotation([2,3,4,5,6],2))
 
 
 
+//CHECK THE STRING IS ANAGRAM                                                                        
+
+// function anagram(one,two){
+//     var a=one.toLowerCase()
+//     var b=two.toLowerCase()
+
+//     a=a.split("").sort().join("")
+//     b=b.split("").sort().join("")
+
+//     return a===b
+// }
+// console.log(anagram("listen","silent"))
+
+                                                                                     
+// const vow="javacsrit"
+// let vowels=["a","e","i","o","u"]
+// let count=0
+// for(let i of vow){
+//     if(vowels.includes(i)){
+//         count++
+//     }
+// }
+// console.log(count)
+                                                                                             
+
+                                                                                
+//TURN OBJECT INTO ARRAY
+// let obj={
+//     id:1,
+//     name:"user1",
+//     age:20,
+//     work:"programmer"
+// }
+
+// console.log(Object.entries(obj))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const b=[1,2,3,4,5,6,7,8,9,10]
+for(var i=0;i<10;i++){
+    setTimeout(()=>{
+        console.log(b[i])
+    },1000)
+}
 
 
 
